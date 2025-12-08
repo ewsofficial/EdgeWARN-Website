94f9a3cc9ffbe2753076efe0bff6ece4364ff1f6
yuchen-wei3667 <weiyu19one@gmail.com>
2025-12-08 16:02:57 -0500

    Add coming soon page when launch edgewarn button is clicked

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 
+app.get('/coming-soon', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'coming-soon.html'));
+});
+
 app.get('/api/status', (req, res) => {
   res.json({ status: 'ok', time: new Date().toISOString() });
 });
